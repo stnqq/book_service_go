ab38b93ffef4981d34725b3e7de43c4ae6c57f3a
Aibar <life33890@gmail.com>
2025-01-16 10:45:37 +0500

    Cleaned code

--- cmd/main.go
+++ cmd/main.go
@@ -9,20 +9,17 @@ import (
 )
 
 func main() {
-	// Инициализация базы данных
 	database, err := db.InitDB()
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	// Инициализация маршрутов
 	r := gin.Default()
 	r.Use(handlers.LoggingMiddleware())
 
 	r.POST("/books", handlers.CreateBook(database))
 	r.GET("/books/:id", handlers.GetBookByID(database))
 
-	// Запуск сервиса
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
